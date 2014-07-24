5484169e55a3acb6c7329c90efc8ac358fb61ebc
kenkeiras <me@codigoparallevar.com>
2014-07-24 14:01:50 +0200

    Hide camera (while it still captures images)

--- android_core/udc_robot_control_android/ros_control/src/main/java/es/udc/fic/android/robot_control/camara/CameraPreviewView.java
+++ android_core/udc_robot_control_android/ros_control/src/main/java/es/udc/fic/android/robot_control/camara/CameraPreviewView.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -34,7 +34,7 @@ import java.util.List;
 
 /**
  * Displays preview frames from the camera.
- * 
+ *
  * @author damonkohler@google.com (Damon Kohler)
  */
 public class CameraPreviewView extends ViewGroup {
@@ -67,6 +67,7 @@ public class CameraPreviewView extends ViewGroup {
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+      setVisibility(View.GONE);
       try {
         if (camera != null) {
           camera.setPreviewDisplay(holder);
